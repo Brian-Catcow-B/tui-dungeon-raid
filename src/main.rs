f269bd4d88bcb2a521b31a6cd2cc75e99542a045
Brian B <brianjbillman@gmail.com>
2023-05-27 12:14:52 -0400

    selection working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
@@ -10,14 +10,11 @@ use dungeon_raid_core::game::{
 use ratatui::{
     backend::{Backend, CrosstermBackend},
     buffer::Buffer,
-    layout::{Constraint, Direction, Layout, Rect},
-    style::{Color, Modifier, Style},
-    text::{Span, Text},
-    widgets::{Block, Borders, Paragraph, Widget},
+    layout::Rect,
+    widgets::Widget,
     Frame, Terminal,
 };
 use std::{error::Error, io, io::prelude::*};
-use unicode_width::UnicodeWidthStr;
 
 const LOG_FILE: &'static str = "log.txt";
 fn clear_log_file() {
@@ -27,31 +24,6 @@ fn clear_log_file() {
 fn log_to_file(msg: &String) {
     let mut file = std::fs::File::options().append(true).create(true).open(LOG_FILE).expect("failed to create file");
     writeln!(&mut file, "{}", msg).expect("failed to write file");
-}
-
-enum InputMode {
-    Normal,
-    Editing,
-}
-
-/// App holds the state of the application
-struct App {
-    /// Current value of the input box
-    input: String,
-    /// Current input mode
-    input_mode: InputMode,
-    /// History of recorded messages
-    messages: Vec<String>,
-}
-
-impl Default for App {
-    fn default() -> App {
-        App {
-            input: String::new(),
-            input_mode: InputMode::Normal,
-            messages: Vec::new(),
-        }
-    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -64,8 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     // create app and run it
-    let app = App::default();
-    let res = run_app(&mut terminal, app);
+    let res = run_app(&mut terminal);
 
     // restore terminal
     disable_raw_mode()?;
@@ -97,8 +68,9 @@ const CURSOR_MAX_RIGHT: u16 = CURSOR_MAX_LEFT + DEFAULT_BOARD_WIDTH as u16 * 2 -
 const CURSOR_MAX_DOWN: u16 = CURSOR_MAX_UP + DEFAULT_BOARD_HEIGHT as u16 * 2 - 1;
 const CURSOR_MAX_LEFT: u16 = 0;
 
-fn move_cursor<B: Backend>(terminal: &mut Terminal<B>, m: CursorMove) -> io::Result<()> {
+fn move_cursor<B: Backend>(terminal: &mut Terminal<B>, m: CursorMove) -> io::Result<(u16, u16)> {
     let mut cursor_pos = terminal.get_cursor()?;
+    log_to_file(&format!("move_cursor called with terminal.get_cursor() giving (x {}, y {})", cursor_pos.0, cursor_pos.1));
     match m {
         CursorMove::Up => {
             if cursor_pos.1 >= CURSOR_MAX_UP + CURSOR_MOVE {
@@ -106,12 +78,12 @@ fn move_cursor<B: Backend>(terminal: &mut Terminal<B>, m: CursorMove) -> io::Res
             }
         }
         CursorMove::Right => {
-            if cursor_pos.0 <= CURSOR_MAX_RIGHT + CURSOR_MOVE {
+            if cursor_pos.0 <= CURSOR_MAX_RIGHT - CURSOR_MOVE {
                 cursor_pos.0 += CURSOR_MOVE;
             }
         }
         CursorMove::Down => {
-            if cursor_pos.1 <= CURSOR_MAX_DOWN + CURSOR_MOVE {
+            if cursor_pos.1 <= CURSOR_MAX_DOWN - CURSOR_MOVE {
                 cursor_pos.1 += CURSOR_MOVE;
             }
         }
@@ -121,17 +93,15 @@ fn move_cursor<B: Backend>(terminal: &mut Terminal<B>, m: CursorMove) -> io::Res
             }
         }
     };
-    log_to_file(&format!("set_cursor being called with (x {}, y {})", cursor_pos.0, cursor_pos.1));
-    terminal.set_cursor(cursor_pos.0, cursor_pos.1);
-    Ok(())
+    Ok(cursor_pos)
 }
 
 fn tile_position_from_cursor_position(cursor_position: (u16, u16)) -> TilePosition {
     log_to_file(&format!("tile_position_from_cursor_position: (cursor.x {}, cursor.y {})", cursor_position.0, cursor_position.1));
     let (x, y) = cursor_position;
     TilePosition::new(
-        (2 * (y - CURSOR_MAX_UP)) as isize,
-        (2 * (x - CURSOR_MAX_LEFT)) as isize,
+        ((y - CURSOR_MAX_UP) / 2) as isize,
+        ((x - CURSOR_MAX_LEFT) / 2) as isize,
     )
 }
 
@@ -206,12 +176,12 @@ impl<'a> Widget for GameWidget<'a> {
     }
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     let mut game = Game::default();
-    terminal.set_cursor(0, 0);
+    let mut cursor_position: (u16, u16) = (0, 0);
     terminal.show_cursor()?;
     loop {
-        terminal.draw(|f| ui(f, &game))?;
+        terminal.draw(|f| ui(f, &game, cursor_position))?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
@@ -223,23 +193,18 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 KeyCode::Char('x') => {
                     game.select_tile(tile_position_from_cursor_position(terminal.get_cursor()?));
                 }
-                KeyCode::Char('h') | KeyCode::Left => move_cursor(terminal, CursorMove::Left)?,
-                KeyCode::Char('j') | KeyCode::Down => move_cursor(terminal, CursorMove::Down)?,
-                KeyCode::Char('k') | KeyCode::Up => move_cursor(terminal, CursorMove::Up)?,
-                KeyCode::Char('l') | KeyCode::Right => move_cursor(terminal, CursorMove::Right)?,
+                KeyCode::Char('h') | KeyCode::Left => cursor_position = move_cursor(terminal, CursorMove::Left)?,
+                KeyCode::Char('j') | KeyCode::Down => cursor_position = move_cursor(terminal, CursorMove::Down)?,
+                KeyCode::Char('k') | KeyCode::Up => cursor_position = move_cursor(terminal, CursorMove::Up)?,
+                KeyCode::Char('l') | KeyCode::Right => cursor_position = move_cursor(terminal, CursorMove::Right)?,
                 _ => {}
             };
         }
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>, game: &Game) {
+fn ui<B: Backend>(f: &mut Frame<B>, game: &Game, cursor_pos: (u16, u16)) {
     let game_widget = GameWidget { game: game };
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints([Constraint::Min(DEFAULT_BOARD_HEIGHT as u16 * 2 - 1)].as_ref())
-        .split(f.size());
 
     f.render_widget(
         game_widget,
@@ -251,53 +216,5 @@ fn ui<B: Backend>(f: &mut Frame<B>, game: &Game) {
         ),
     );
 
-    //let (msg, style) = match app.input_mode {
-    //    InputMode::Normal => (
-    //        vec![
-    //            Span::raw("Press "),
-    //            Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-    //            Span::raw(" to exit, "),
-    //            Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-    //            Span::raw(" to start editing."),
-    //        ],
-    //        Style::default().add_modifier(Modifier::RAPID_BLINK),
-    //    ),
-    //    InputMode::Editing => (
-    //        vec![
-    //            Span::raw("Press "),
-    //            Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-    //            Span::raw(" to stop editing, "),
-    //            Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-    //            Span::raw(" to record the message"),
-    //        ],
-    //        Style::default(),
-    //    ),
-    //};
-    //let mut text = Text::from("foo");
-    //text.patch_style(style);
-    //let help_message = Paragraph::new(text);
-    //f.render_widget(help_message, chunks[0]);
-
-    //let input = Paragraph::new(app.input.as_str())
-    //    .style(match app.input_mode {
-    //        InputMode::Normal => Style::default(),
-    //        InputMode::Editing => Style::default().fg(Color::Yellow),
-    //    })
-    //    .block(Block::default().borders(Borders::ALL).title("Input"));
-    //f.render_widget(input, chunks[1]);
-    //match app.input_mode {
-    //    InputMode::Normal =>
-    //        // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-    //        {}
-    //
-    //        InputMode::Editing => {
-    //            // Make the cursor visible and ask ratatui to put it at the specified coordinates after rendering
-    //            f.set_cursor(
-    //                // Put cursor past the end of the input text
-    //                chunks[1].x + app.input.width() as u16 + 1,
-    //                // Move one line down, from the border to the input line
-    //                chunks[1].y + 1,
-    //            )
-    //        }
-    //    }
+    f.set_cursor(cursor_pos.0, cursor_pos.1);
 }
